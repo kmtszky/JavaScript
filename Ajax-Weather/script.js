e90dfe6bd3e3b1a8ad702314e0d7682600ebd0c6
kmtszky <kmtszk711@gmail.com>
2021-04-28 18:33:52 +0900

    [update] script.js => 成功処理

--- Ajax-Weather/script.js
+++ Ajax-Weather/script.js
@@ -7,6 +7,14 @@ $(function() {
       dataType: 'jsonp',
     }).done(function (data) {
       // 通信成功
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
     }).fail(function (data) {
       // 通信失敗
     });
